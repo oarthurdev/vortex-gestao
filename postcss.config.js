d69a7947399c50346906082c1954cf7f0a3f93bf
Arthur Wagenknecht <oarthurdev@gmail.com>
2026-01-19 00:42:28 -0300

    feat(repoai): Adicionar uma documentação completa - postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
-export default {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-}
+# Projeto CSS com Tailwind e Autoprefixer
+
+Este projeto utiliza PostCSS com Tailwind CSS e Autoprefixer para manuseio e processamento de estilos. 
+
+## Instalação
+
+1. **Clone o repositório**
